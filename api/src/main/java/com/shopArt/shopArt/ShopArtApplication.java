2bceab7e3d3594bcf1517b5d40e81b20e874c6b7
Patryk Kożuchowski <patryk.kozuchowski11@gmail.com>
2020-09-05 11:49:57 +0200

    Initial commit

---
+++ api/src/main/java/com/shopArt/shopArt/ShopArtApplication.java
@@ -0,0 +1,13 @@
+package com.shopArt.shopArt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopArtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopArtApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/shopArt/shopArt/ShopArtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopArt.shopArt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopArtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
